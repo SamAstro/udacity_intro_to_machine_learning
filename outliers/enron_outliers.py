da24d8d21a895f3c191d2772671f54a3ce516b23
Samia Drappeau <drappeau.samia@gmail.com>
2016-09-20 15:26:59 +0200

    Finish enron outliers project.

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,42 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+# Looks like the outlier numbers match that of the total in the pdf file. Let's
+# check that
+
+print data_dict['TOTAL']['salary']
+print data_dict['TOTAL']['bonus']
+# Outputs :
+#   26704229
+#   97343619
+# We have a match.
+
+
+# Remove the outlier
+data_dict.pop('TOTAL', 0)
+
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+# Look for the bandits
+for k, v in data_dict.iteritems():
+    if data_dict[k]['salary'] == 'NaN' or data_dict[k]['bonus'] == 'NaN':
+        pass
+    else:
+        if data_dict[k]['bonus'] >= 5e6 and data_dict[k]['salary'] >= 1e6:
+            print k
+            print data_dict[k]['bonus']
+            print data_dict[k]['salary']
 
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
